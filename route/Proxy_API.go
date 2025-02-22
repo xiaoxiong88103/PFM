95dbffd89739a992d4c673efdc48a77c9e544b57
德山 <2694065818@qq.com>
2025-02-22 10:19:59 +0800

    feat: async golang backend whitelist codes

--- route/Proxy_API.go
+++ route/Proxy_API.go
@@ -25,6 +25,8 @@ func Proxy_Route(r *gin.Engine) {
 	whiteList.POST("/add", WhiteList.AddWhiteListHandler)
 	// 查看白名单路由
 	whiteList.GET("/list", WhiteList.ViewWhiteListHandler)
+	// 查看全部白名单路由
+	whiteList.GET("/list/all", WhiteList.ViewAllWhiteListsHandler)
 	// 删除白名单路由
 	whiteList.POST("/delete", WhiteList.DeleteWhiteListHandler)
 
